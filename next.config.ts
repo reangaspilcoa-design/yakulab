7db825084fabef2bb28e5e5a81937d5abbc3e16f
reangaspilcoa-design <reangaspilcoa@gmail.com>
2026-07-14 12:24:26 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./prisma/**/*'],
+    },
+  },
+}
 
 export default nextConfig
