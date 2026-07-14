175211c0582d624ddb6baa57882ea04249a43a63
reangaspilcoa-design <reangaspilcoa@gmail.com>
2026-07-14 12:32:08 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      '/api/**/*': ['./prisma/**/*'],
-    },
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./prisma/**/*'],
   },
 }
 
